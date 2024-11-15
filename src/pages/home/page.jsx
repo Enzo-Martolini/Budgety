ea6eae3c37839c43664e7674ecc39e46faddaf02
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-11-15 14:25:06 +0100

    homepage connected to localstorage

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,26 +1,21 @@
 import { CardTransaction } from "./card-transaction";
 import { Chart } from "./chart";
+import * as functions from '../../functions/functions';
 
 export const HomePage = () => {
+    const trading = functions.getFromLocalStorage("trading")
   return (
+    <>
+    <Chart />
     <div className="grid lg:grid-cols-2 m-7">
-      <Chart />
-      <CardTransaction name="Transaction" price={1650.84} symbole="€" />
+      {trading.map((trade, index) =>( 
+        <>
+        <CardTransaction name={trade.name} price={trade.rising} symbole="€" key={index}/>
+        </>)
+      )}
       <br></br>
-      <CardTransaction name="Transaction" price={-650} symbole="€" />
-      <br></br>
-      <CardTransaction name="Transaction" price={-124.56} symbole="€" />
-      <br></br>
-      <CardTransaction name="Transaction" price={-59.99} symbole="€" />
-      <br></br>
-      <CardTransaction name="Transaction" price={100} symbole="€" />
-      <br></br>
-      <CardTransaction name="Transaction" price={-9.90} symbole="€" />
-      <br></br>
-      <CardTransaction name="Transaction" price={-1.99} symbole="€" />
-      <br></br>
-      <CardTransaction name="Transaction" price={-32.50} symbole="€" />
       <br></br> 
     </div>
+    </>
   );
 };
